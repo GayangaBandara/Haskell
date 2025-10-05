3dbcce75a5437c757a03aeec37c7a7d3cb73ea4d
GayangaDev <gayangabandara74@gmail.com>
2025-10-05 17:47:48 +0530

    fix that and remove the minor shadowing warning

--- Sum_of_Multiples/sum_of_multiples.hs
+++ Sum_of_Multiples/sum_of_multiples.hs
@@ -1,14 +1,13 @@
-module EnergyPoints (calculateEnergyPoints) where
+module SumOfMultiples (sumOfMultiples) where
 
 import Data.List (nub)
 
--- Calculates all multiples of a number less than the limit
-multiplesBelow :: Int -> Int -> [Int]
-multiplesBelow base limit = [x | x <- [1..(limit - 1)], x `mod` base == 0]
-
--- Main function: calculates energy points for a given level and magical item base values
-calculateEnergyPoints :: Int -> [Int] -> Int
-calculateEnergyPoints level bases =
-  let allMultiples = concatMap (`multiplesBelow` level) bases
-      uniqueMultiples = nub allMultiples
-  in sum uniqueMultiples
+-- | Calculates the sum of all unique multiples of given factors below a given limit.
+sumOfMultiples :: [Integer] -> Integer -> Integer
+sumOfMultiples factors limit =
+    sum . nub . concatMap multiplesBelow $ factors
+  where
+    multiplesBelow :: Integer -> [Integer]
+    multiplesBelow n
+        | n <= 0    = []  -- ignore zero or negative factors
+        | otherwise = [x | x <- [n, 2*n .. limit - 1]]
